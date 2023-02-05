e8e5b80f1e3cf15ac578cdce64ac47ac57f47a85
Jan-Kiezun <j4n3k777@gmail.com>
2023-02-05 14:42:01 +0100

    added crud for comments and rating, added get popular

--- api/src/controllers/movies.js
+++ api/src/controllers/movies.js
@@ -7,9 +7,7 @@ const driver = neo4j.driver(
 );
 
 const session = driver.session();
-//
-// GET /movies
-//
+
 const getAllMovies = async (page) => {
   const result = await session.run(
     `MATCH (movie:Movie) RETURN movie ${
@@ -28,12 +26,72 @@ const getAllMovies = async (page) => {
   return { page: page, movies };
 };
 
-const searchMovies = async (query, page) => {
+const getMovie = async (id) => {
   const result = await session.run(
-    `MATCH (movie:Movie) WHERE toLower(movie.title) CONTAINS toLower($query) RETURN movie ${
+    `match (u:User)-[r:RATED]->(m:Movie {id: $id}) return u,r,m`,
+    { id: id }
+  );
+  const ratings = result.records.map((record) => {
+    return {
+      user_id: record._fields[0].properties.id,
+      rating: record._fields[1].properties.rating,
+      timestamp: record._fields[1].properties.timestamp,
+    };
+  });
+  const movie = result.records[0]._fields[2].properties;
+  const comments = await getComments(id);
+  const allInfo = { ratings: ratings, comments: comments, movie: movie };
+  return allInfo;
+};
+
+const searchMovies = async (phrase, page, genre, sort_field, asc) =>
+  new Promise((resolve, reject) => {
+    const whereClause = `WHERE toLower(m.genres) CONTAINS toLower($genre) AND toLower(m.title) CONTAINS toLower($phrase)`;
+    const sortClause = sort_field ? `ORDER BY m.${sort_field} ${asc}` : "";
+    const paginate = page > 1 ? `SKIP ${(page - 1) * 20}` : "";
+    const query = `MATCH (m:Movie) ${whereClause} RETURN m ${sortClause} ${paginate} LIMIT 20`;
+    session
+      .run(query, { phrase: phrase, genre: genre })
+      .then((result) => {
+        const movies = result.records.map((record) => {
+          return {
+            id: record._fields[0].properties.id,
+            tmdbId: record._fields[0].properties.tmdbId,
+            title: record._fields[0].properties.title,
+            tagline: record._fields[0].properties.tagline,
+            released: record._fields[0].properties.release_date,
+          };
+        });
+        resolve({ page: page, movies });
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+
+const checkIfIdExists = async (id) => {
+  const result = await session.run(`MATCH (m:Movie {id: $id}) RETURN m`, {
+    id: id,
+  });
+  return result.records.length > 0;
+};
+const getPages = new Promise((resolve, reject) => {
+  session
+    .run(`MATCH (movie:Movie) RETURN count(movie)`)
+    .then((result) => {
+      const count = result.records[0]._fields[0].low;
+      resolve(Math.ceil(count / 20));
+    })
+    .catch((error) => {
+      reject(error);
+    });
+});
+
+const getPopularMovies = async (page) => {
+  const result = await session.run(
+    `MATCH (movie:Movie) RETURN movie ORDER BY movie.popularity DESC ${
       page > 1 ? `SKIP ${(page - 1) * 20}` : ""
-    } LIMIT 20`,
-    { query: query }
+    } LIMIT 20`
   );
   const movies = result.records.map((record) => {
     return {
@@ -42,12 +100,122 @@ const searchMovies = async (query, page) => {
       title: record._fields[0].properties.title,
       tagline: record._fields[0].properties.tagline,
       released: record._fields[0].properties.release_date,
+      popularity: record._fields[0].properties.popularity,
     };
   });
   return { page: page, movies };
 };
 
+const addMovie = async (movie) => {
+  const {
+    title,
+    tagline,
+    release_date,
+    genres,
+    overview,
+    vote_average,
+    vote_count,
+    popularity,
+  } = movie;
+  let id = Math.floor(Math.random() * 100000000000000000) + "";
+  while (await checkIfIdExists(id)) {
+    id = Math.floor(Math.random() * 100000000000000000) + "";
+  }
+  console.log(movie);
+  console.log(overview);
+  const query = `CREATE (m:Movie {
+    id: $id,
+    title: $title,
+    tagline: $tagline,
+    release_date: $release_date,
+    genres: $genres,
+    overview: $overview,
+    vote_average: $vote_average,
+    vote_count: $vote_count,
+    popularity: $popularity
+  }) RETURN m`;
+  const params = {
+    id: id,
+    ...movie,
+  };
+
+  const result = await session.run(query, params);
+  return result;
+};
+
+const addComment = async (comment) => {
+  const { movieId, userId, text, timestamp } = comment;
+  const query = `MATCH (m:Movie {id: $movieId}), (u:User {id: $userId})
+  CREATE (u)-[r:COMMENTED {text: $text, timestamp: $timestamp}]->(m)
+  RETURN r`;
+  const params = {
+    movieId,
+    userId,
+    text,
+    timestamp,
+  };
+  const result = await session.run(query, params);
+  return result.records[0]._fields[0].properties;
+};
+
+const getComments = async (movieId) => {
+  const query = `MATCH (u:User)-[r:COMMENTED]->(m:Movie {id: $movieId})
+  RETURN u,r,m`;
+  const params = {
+    movieId,
+  };
+  const result = await session.run(query, params);
+  const comments = result.records.map((record) => {
+    return {
+      user_id: record._fields[0].properties.id,
+      text: record._fields[1].properties.text,
+      timestamp: record._fields[1].properties.timestamp,
+    };
+  });
+  return comments;
+};
+
+const setRating = async (rating_data) => {
+  const { movieId, userId, rating, timestamp } = rating_data;
+  const query = `MATCH (m:Movie {id: $movieId}), (u:User {id: $userId})
+  MERGE (u)-[r:RATED]->(m)
+  SET r.rating = $rating, r.timestamp = $timestamp`;
+  const params = {
+    movieId,
+    userId,
+    rating,
+    timestamp,
+  };
+  const result = await session.run(query, params);
+  console.log(result);
+  return result.records[0]?._fields[0].properties || "Rating upadted";
+};
+
+const getRatings = async (movieId) => {
+  const query = `MATCH (u:User)-[r:RATED]->(m:Movie {id: $movieId})
+  RETURN u,r,m`;
+  const params = {
+    movieId,
+  };
+  const result = await session.run(query, params);
+  const ratings = result.records.map((record) => {
+    return {
+      user_id: record._fields[0].properties.id,
+      rating: record._fields[1].properties.rating,
+      timestamp: record._fields[1].properties.timestamp,
+    };
+  });
+  return ratings;
+};
+
 module.exports = {
   getAllMovies,
+  getMovie,
   searchMovies,
+  getPopularMovies,
+  addMovie,
+  addComment,
+  getComments,
+  setRating,
+  getRatings,
 };
--- api/src/routes/movies.js
+++ api/src/routes/movies.js
@@ -1,6 +1,19 @@
-﻿const express = require("express");
+﻿require("dotenv").config();
+const express = require("express");
 const router = express.Router();
-const { getAllMovies, searchMovies } = require("../controllers/movies");
+const {
+  getAllMovies,
+  getMovie,
+  searchMovies,
+  getPopularMovies,
+  addMovie,
+  addComment,
+  getComments,
+  setRating,
+  getRatings,
+} = require("../controllers/movies");
+const API_KEY = process.env.API_KEY;
+const axios = require("axios");
 
 router.get("/", async (req, res) => {
   const page = parseInt(req.query.page) || 1;
@@ -8,11 +21,57 @@ router.get("/", async (req, res) => {
   res.send(movies);
 });
 
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const movie = await getMovie(id);
+  res.send(movie);
+});
+
 router.get("/search", async (req, res) => {
-  const query = req.query.q;
+  const query = req.query.q || "";
   const page = parseInt(req.query.page) || 1;
-  const movies = await searchMovies(query, page);
+  const genre = req.query.genre || "";
+  const sort_field = req.query.sort || "";
+
+  const asc = (sort_field && req.query.asc) || "ASC";
+  const movies = await searchMovies(query, page, genre, sort_field, asc);
   res.send(movies);
+});
+
+router.get("/popular", async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const movies = await getPopularMovies(page);
+  res.send(movies);
+});
+
+router.post("/add", async (req, res) => {
+  const movie = req.body;
+  const result = await addMovie(movie);
+  res.send({ message: "Movie added", result });
+});
+
+router.post("/comments/add", async (req, res) => {
+  const comment = req.body;
+  const result = await addComment(comment);
+  res.send({ message: "Comment added", result });
+});
+
+router.get("/comments/:id", async (req, res) => {
+  const id = req.params.id;
+  const comments = await getComments(id);
+  res.send(comments);
+});
+
+router.put("/ratings/set", async (req, res) => {
+  const rating = req.body;
+  const result = await setRating(rating);
+  res.send({ result });
+});
+
+router.get("/ratings/:id", async (req, res) => {
+  const id = req.params.id;
+  const ratings = await getRatings(id);
+  res.send(ratings);
 });
 
 module.exports = router;
