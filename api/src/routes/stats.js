5ed91d1f5e6dc5da9fd630a781afc560fe1edd6c
Jan-Kiezun <j4n3k777@gmail.com>
2023-02-07 02:18:49 +0100

    added stats for movies and their ratings

--- api/src/routes/stats.js
+++ api/src/routes/stats.js
@@ -1,15 +1,6 @@
 ﻿require("dotenv").config();
 const express = require("express");
 const router = express.Router();
-/*
-Most popular movie
-Least popular movie
-Highest rated movie
-Lowest rated movie
-Movie with the most ratings
-Most popular genre
-Least popular genre
-*/
 const {
   getPopularMovie,
   getUnPopularMovie,
@@ -20,65 +11,34 @@ const {
   getLeastPopularGenre,
 } = require("../controllers/stats");
 
-router.get("/mostPopularMovie", async (req, res) => {
+router.get("/getStats", async (req, res) => {
   try {
-    const movie = await getPopularMovie();
-    res.send(movie);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
-  }
-});
-
-router.get("/leastPopularMovie", async (req, res) => {
-  try {
-    const movie = await getUnPopularMovie();
-    res.send(movie);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
-  }
-});
-router.get("/highRatedMovie", async (req, res) => {
-  try {
-    const movie = await getHighRatedMovie();
-    res.send(movie);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
-  }
-});
-router.get("/lowRatedMovie", async (req, res) => {
-  try {
-    const movie = await getLowRatedMovie();
-    res.send(movie);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
-  }
-});
-router.get("/mostRatingsMovie", async (req, res) => {
-  try {
-    const movie = await getMostRatingsMovie();
-    res.send(movie);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
-  }
-});
-router.get("/mostPopularGenre", async (req, res) => {
-  try {
-    const movie = await getMostPopularGenre();
-    res.send(movie);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err);
-  }
-});
-router.get("/leastPopularGenre", async (req, res) => {
-  try {
-    const movie = await getLeastPopularGenre();
-    res.send(movie);
+    const [
+      popularMovie,
+      unPopularMovie,
+      highRatedMovie,
+      lowRatedMovie,
+      mostRatingsMovie,
+      mostPopularGenre,
+      leastPopularGenre,
+    ] = await Promise.all([
+      getPopularMovie(),
+      getUnPopularMovie(),
+      getHighRatedMovie(),
+      getLowRatedMovie(),
+      getMostRatingsMovie(),
+      getMostPopularGenre(),
+      getLeastPopularGenre(),
+    ]);
+    res.send({
+      popularMovie,
+      unPopularMovie,
+      highRatedMovie,
+      lowRatedMovie,
+      mostRatingsMovie,
+      mostPopularGenre,
+      leastPopularGenre,
+    });
   } catch (err) {
     console.log(err);
     res.status(500).send(err);
